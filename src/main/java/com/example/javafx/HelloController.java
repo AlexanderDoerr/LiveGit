a25bb2de343ed578d881f48688b61a114512c100
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2022-07-27 11:31:10 -0600

    New project

--- src/main/java/com/example/javafx/HelloController.java
+++ src/main/java/com/example/javafx/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.javafx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
@@ -8,7 +9,19 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    private Button btnHello;
+    @FXML
+    private Button btnClickCount;
+    int counter = 0;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        btnHello.setText("Done!");
+    }
+
+    @FXML
+    protected void onClickCounter(){
+        btnClickCount.setText("" + counter++);
     }
 }
