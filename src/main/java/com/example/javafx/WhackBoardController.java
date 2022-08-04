41deb1f32613e4cdf925bc846cc1e358f9050cbb
AlexanderDoerr <99917682+AlexanderDoerr@users.noreply.github.com>
2022-08-04 14:57:26 -0600

    Completed logic for mole's moving up or down and whack logic working.

--- src/main/java/com/example/javafx/WhackBoardController.java
+++ src/main/java/com/example/javafx/WhackBoardController.java
@@ -15,11 +15,9 @@ import java.util.*;
 public class WhackBoardController implements Initializable {
 
     //private boolean[][] aryUp= {{false, false, false}, {false, false, false,}, {false, false, false}};
-    private String moleUp = "file:/C:/Users/Alexander%20Doerr/Neumont%20School%20Folder/Q4/CSC180/JavaFX/src/main/resources/com/example/javafx/MoleUp.PNG";
-    private String moleDown = "file:/C:/Users/Alexander%20Doerr/Neumont%20School%20Folder/Q4/CSC180/JavaFX/src/main/resources/com/example/javafx/MoleDown.PNG";
-    private ImageView imageUp = new ImageView(moleUp);
-    private ImageView imageDown = new ImageView(moleDown);
-
+    private final String moleUp = "file:/C:/Users/Alexander%20Doerr/Neumont%20School%20Folder/Q4/CSC180/JavaFX/src/main/resources/com/example/javafx/MoleUp.PNG";
+    private final String moleDown = "file:/C:/Users/Alexander%20Doerr/Neumont%20School%20Folder/Q4/CSC180/JavaFX/src/main/resources/com/example/javafx/MoleDown.PNG";
+    private final String moleWhack = "file:/C:/Users/Alexander%20Doerr/Neumont%20School%20Folder/Q4/CSC180/JavaFX/src/main/resources/com/example/javafx/MoleWhack.jpg";
 
     @FXML
     private ImageView img00;
@@ -48,7 +46,7 @@ public class WhackBoardController implements Initializable {
     @FXML
     private ImageView img22;
 
-    private ImageView[][] images = { {img00, img01, img02}, {img10, img11, img12}, {img20, img21, img22} };
+
     Random rand = new Random();
 
     @FXML
@@ -56,34 +54,23 @@ public class WhackBoardController implements Initializable {
     {
         try {
             ImageView source = (ImageView) event.getSource();
+            ImageView tempImg = (ImageView) event.getTarget();
             System.out.println(source.getImage().getUrl());// this grabs the image location and maybe able to just compare to see if its up or down
 
+            Image image = new Image(moleWhack);
 
-            InputStream stream = new FileInputStream("src\\main\\resources\\com\\example\\javafx\\MoleUp.png");
-            Image image = new Image(moleUp);
-            img22.setImage(image);
-            // compare the image to the source like image up or down
-            //You might need to set a path for up and down so everytime you change the picture you change it to the original path
-            Random rand = new Random();
-            int intRandom = rand.nextInt(3);
-            System.out.println("Random num: " + intRandom);
-
-
-//            System.out.println(event.getTarget());
-            //img00.setId("Up");// You might be able to use this so you can always
-//            System.out.println(img00.getId());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            //event.getSource();
-        }
-
+            if (moleUp.equals(source.getImage().getUrl()))
+            {
+                (tempImg).setImage(image);
+            }
+        } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
     }
 
-    public void moleMovement(Image up, Image down)
+    public void moleMovement(ImageView[][] images, Image up, Image down)
     {
-//        int intRandom = rand.nextInt(3);
-
         for (int i = 0; i < images.length; i++)
         {
 
@@ -98,44 +85,17 @@ public class WhackBoardController implements Initializable {
                     default:
                         System.out.println("Somthing went wrong at moleMovement");
                 }
-                System.out.println(images[i][j] + "\n");
+                //System.out.println(images[i][j] + "\n");
             }
         }
     }
 
-
-//    @FXML
-//    void img00_Clicked(MouseEvent event) throws FileNotFoundException {
-//
-//        try {
-//            InputStream stream = new FileInputStream("src\\main\\resources\\com\\example\\javafx\\MoleUp.png");
-//            Image image = new Image(stream);
-//            img00.setImage(image);
-//            ImageView source = (ImageView) event.getSource();
-//            System.out.println(source.getImage().getUrl());// this grabs the image location and maybe able to just compare to see if its up or down
-//            // compare the image to the source like image up or down
-//            //You might need to set a path for up and down so everytime you change the picture you change it to the original path
-//            Random rand = new Random();
-//            int intRandom = rand.nextInt(3);
-//            System.out.println("Random num: " + intRandom);
-//
-//            System.out.println(event.getTarget());
-//            //img00.setId("Up");// You might be able to use this so you can always
-//            System.out.println(img00.getId());
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            //event.getSource();
-//        }
-//    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        ImageView[][] images = { {this.img00, this.img01, this.img02}, {this.img10, this.img11, this.img12}, {this.img20, this.img21, this.img22} };
 
         Image imageUp = new Image(moleUp);
         Image imageDown = new Image(moleDown);
-
-//        molesDown(imageDown);
 
         Timer t = new Timer();
         TimerTask task = new TimerTask()
@@ -143,24 +103,11 @@ public class WhackBoardController implements Initializable {
             @Override
             public void run()
             {
-//                molesDown(imageDown);
-//                moleMovement(imageUp, imageDown);
-                //This is where you put your methods to swap the moles for every 2 seconds
-                System.out.println("Timer ticked " + new Date() );
+                moleMovement(images, imageUp, imageDown);
+                //System.out.println("Timer ticked " + new Date() );
             }
         };
-        t.schedule(task, new Date(), 2000);
+        t.schedule(task, new Date(), 1000);
 
-    }
-
-    public void molesDown(Image down)
-    {
-        for (int i = 0; i < images.length; i++) {
-            for (int j = 0; j < images[i].length; j++) {
-                //img00.setImage(down); this works just not in the array
-                images[i][j].setImage(down);
-            }
-
-        }
     }
 }
